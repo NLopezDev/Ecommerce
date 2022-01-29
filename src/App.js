3427ab26ae4b257e3f0ff64cb9f9f5aaa8321f62
ServiWeb <nlopez.developer@outlook.com>
2022-01-28 22:09:21 -0300

    Se agrego detalles de la web

--- src/App.js
+++ src/App.js
@@ -5,25 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <nav>
-                <ul class="nav justify-content-center">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#ini">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#nosot">Nosotros</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#prod">Productos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#cont" tabindex="-1" aria-disabled="true">Contacto</a>
-                    </li>
-                </ul>
-            </nav>
-
+      
         <img src={macarons} className="App-logo" alt="macarons" />
-        
+
+        <h1>Nuestra historia</h1>
+                    <p>En el año 2019 nace un sueño. El sueño de una emprendedora que querían ofrecer algo diferente, un concepto distinto en pastelería. Nuestro objetivo es ofrecer un producto de alta calidad, excelente presentación, pero con el sabor de
+                        lo casero.</p>
+                    <p>En la actualidad, desarrollamos, elaboramos y comercializamos productos de pastelería y panificados.</p>
+                    <p>Los invitamos a descubrir las delicias de nuestra carta.</p><br></br>
         <a
           className="App-link"
           href="https://reactjs.org"
